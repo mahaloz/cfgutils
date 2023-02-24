861d5163f06289bbebbb5740246f0e454c24978f
mahaloz <zionbasque@gmail.com>
2023-02-24 16:26:09 -0700

    fix readme

--- tests.py
+++ tests.py
@@ -9,18 +9,18 @@ from cfgutils.regions.region_identifier import RegionIdentifier
 
 class TestRegionIdentification(unittest.TestCase):
     def test_region_identification(self):
-        """
-        Graph:
-                1
-               / \
-              2   3
-              \   /
-                4
-               / \
-              5   6
-               \ /
-                7
-        """
+        #
+        # Graph:
+        #         1
+        #        / \
+        #       2   3
+        #       \   /
+        #         4
+        #        / \
+        #       5   6
+        #        \ /
+        #         7
+        #
         blocks = [None] + [GenericBlock(i) for i in range(1, 8)] + [None]
         numbered_edges = [(1, 2), (1, 3), (2, 4), (3, 4), (4, 5), (4, 6), (5, 7), (6, 7)]
         block_edges = [
@@ -29,6 +29,8 @@ class TestRegionIdentification(unittest.TestCase):
         graph = nx.DiGraph(block_edges)
         ri = RegionIdentifier(graph)
         top_region = ri.region
+
+        # TODO: add more checks for the other regions
         self.assertTrue(top_region.head.head.head, blocks[1])
         self.assertIn((top_region.head, blocks[7]), ri.region.graph.edges)
 
